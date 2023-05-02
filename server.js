e107422bee0c58b106f17d74079dbaa5919826e5
wks admin <wks.admin@COLENTINA.local>
2023-05-02 21:21:49 +0300

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/product'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', {root: 'dist/amgular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/amgular-heroku/'})
 );
 
 app.listen(process.env.PORT || 8080);
